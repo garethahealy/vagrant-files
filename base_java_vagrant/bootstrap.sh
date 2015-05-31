22e60e1b9afee247426361c676e1a7976ccbdec2
Gareth Healy <garethahealy@gmail.com>
2015-05-31 21:04:55 +0100

    Added env files Added java install

--- base_java_vagrant/bootstrap.sh
+++ base_java_vagrant/bootstrap.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-sudo yum clean all && yum -y update && yum install -y wget vim zip unzip
+# Set debug mode and halt on errors
+set -x
+set -e
+
+sudo yum clean all && \
+     yum install -y epel-release && \
+     yum -y update && \
+     yum install -y java-1.7.0-openjdk.x86_64 wget vim zip unzip sshpass
+
+# Set debug mode off
+set +x
