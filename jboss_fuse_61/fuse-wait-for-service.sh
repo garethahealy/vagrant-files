b9f8e5b27740696f4bc3fc6aeadf8eb07eb19f65
Gareth Healy <garethahealy@gmail.com>
2015-06-03 20:46:30 +0100

    Trying to get bash script to work with alias

--- jboss_fuse_61/fuse-wait-for-service.sh
+++ jboss_fuse_61/fuse-wait-for-service.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+shopt -s expand_aliases
+
 # Set debug mode and halt on errors
 set -x
 set -e
@@ -7,10 +9,12 @@ set -e
 # Full path of your ssh, used by the aliases
 SSH_PATH=$(which ssh)
 
+#ssh2fabric() {
+#    sshpass -p admin $SSH_PATH -p 8101 -o ServerAliveCountMax=100 -o ConnectionAttempts=180 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o LogLevel=ERROR admin@localhost "$@"
+#}
+
 # Function to connect to the ssh server exposed by JBoss Fuse. Uses sshpass to script the password authentication
-ssh2fabric() {
-    sshpass -p admin $SSH_PATH -p 8101 -o ServerAliveCountMax=100 -o ConnectionAttempts=180 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o LogLevel=ERROR admin@localhost "$@"
-}
+alias ssh2fabric="sshpass -p admin $SSH_PATH -p 8101 -o ServerAliveCountMax=100 -o ConnectionAttempts=180 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o LogLevel=ERROR admin@localhost"
 
 # wait for ssh server to be up, avoids "Connection reset by peer" errors
 while ! ssh2fabric "echo up" ; do sleep 1s; done;
@@ -19,7 +23,7 @@ while ! ssh2fabric "echo up" ; do sleep 1s; done;
 ssh2fabric "wait-for-service -t 300000 io.fabric8.api.BootstrapComplete"
 
 # Scaffold fuse
-# ssh2fabric "shell:source /home/vagrant/scaffold-fuse-fabric.karaf"
+ssh2fabric "shell:source /home/vagrant/scaffold-fuse-fabric.karaf"
 
 # Set debug mode off
 set +x
