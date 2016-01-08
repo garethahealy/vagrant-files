8d467a6968e42711f21e4d00ccab58f2480731d2
Gareth Healy <garethahealy@gmail.com>
2016-01-08 22:21:23 +0000

    Added config for postgres Started to add config for jon server

--- jboss_on/jboss_on_33/bootstrap-jon-server.sh
+++ jboss_on/jboss_on_33/bootstrap-jon-server.sh
@@ -2,4 +2,29 @@
 
 set -x
 
+# Create JON user
+sudo adduser jon
+echo "jon" | sudo passwd "jon" --stdin
 
+# Install JON and apply patches
+cd /opt/rh &&
+    unzip jon-server-3.3.0.GA.zip &&
+    unzip jon-server-patch-3.3-update-04.zip
+    jon-server-3.3.0.GA-update-04/apply-updates.sh /opt/rh/jon-server-3.3.0.GA
+
+# Remove files before hotfix
+cd /opt/rh/jon-server-3.3.0.GA &&
+    rm -f jbossas/modules/system/layers/base/org/apache/commons/collections/main/commons-collections-3.2.1.redhat-3.jar &&
+    rm -f jbossas/modules/system/layers/base/org/apache/commons/collections/main/module.xml &&
+    rm -f modules/org/rhq/server-startup/main/deployments/rhq.ear/rhq-portal.war/WEB-INF/lib/commons-collections-3.2.1.jar &&
+    rm -f modules/org/rhq/server-startup/main/deployments/rhq.ear/rhq-content_http.war/WEB-INF/lib/commons-collections-3.2.1.jar &&
+    rm -f modules/org/rhq/server-startup/main/deployments/rhq.ear/lib/commons-collections-3.2.1.jar
+
+# Apply hotfix
+cd /opt/rh
+    unzip -od /opt/rh/jon-server-3.3.0.GA patch-common-collections-BZ-1281514.zip
+
+sudo chown -R jon:jon /opt/rh/jon-server-3.3.0.GA
+sudo -u jon /opt/rh/jon-server-3.3.0.GA/bin/rhqctl install --start
+
+## todo: need to provide options
--- jboss_on/jboss_on_33/bootstrap-postgres.sh
+++ jboss_on/jboss_on_33/bootstrap-postgres.sh
@@ -2,4 +2,29 @@
 
 set -x
 
+# Install postgres
+sudo yum install -y postgresql-server &&
+    sudo postgresql-setup initdb &&
+    sudo systemctl enable postgresql.service
+    sudo systemctl start postgresql.service
 
+# Change password for postgres user
+echo "postgres" | sudo passwd "postgres" --stdin
+
+# Create rhq db
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
+sudo -u postgres psql -c "CREATE USER rhqadmin PASSWORD 'rhqadmin';"
+sudo -u postgres psql -c "CREATE DATABASE rhq OWNER rhqadmin;"
+
+# Change auth to be md5 based
+sudo sed -i "s/local   all             all                                     peer/local   all             all                                     md5/" /var/lib/pgsql/data/pg_hba.conf
+sudo sed -i "s/host    all             all             127.0.0.1\/32            ident/host    all             all             127.0.0.1\/32            md5/" /var/lib/pgsql/data/pg_hba.conf
+sudo sed -i "s/host    all             all             ::1\/128                 ident/host    all             all             ::1\/128                 md5/" /var/lib/pgsql/data/pg_hba.conf
+sudo sh -c "echo 'host    all             all             10.20.3.2/24            md5' >> /var/lib/pgsql/data/pg_hba.conf"
+
+# Reload config postgres
+sudo systemctl restart postgresql.service
+
+# Check rhqadmin user works
+export PGPASSWORD=rhqadmin &&
+    psql -U rhqadmin -d rhq
