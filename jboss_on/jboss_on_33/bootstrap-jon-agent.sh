b420374855d51f8ae3c61f46c84fe855d0604085
Gareth Healy <garethahealy@gmail.com>
2016-01-08 22:23:26 +0000

    Added config to install fuse with basic set of containers

--- jboss_on/jboss_on_33/bootstrap-jon-agent.sh
+++ jboss_on/jboss_on_33/bootstrap-jon-agent.sh
@@ -2,4 +2,11 @@
 
 set -x
 
+# Run scaffolding scripts to install JBoss Fuse
+cd /tmp &&
+    unzip scaffolding-scripts.zip &&
+    cd scripts &&
+    chmod -R 755 *.sh &&
+    ./install-fuse-and-deploy.sh -e vagrant-child -u vagrant
 
+##todo - get agent and install
